cc893df5627fed3521bdd40f6660ab403838e3d9
KedarisettiSreeVamsi <34485436+KedarisettiSreeVamsi@users.noreply.github.com>
2019-12-30 15:43:06 +0530

    Update gitam_automate.py

--- gitam_automate.py
+++ gitam_automate.py
@@ -1,4 +1,3 @@
-# Selenium Module(Automates Browser)
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,9 +8,6 @@ from tkinter import *
 import tkinter.messagebox
 import requests,os,time
 from zipfile import ZipFile
-import platform
-import os
-from pip._internal.utils.misc import get_installed_distributions
 
 window = Tk()
 window.title('Gitam Univeristy Semester Result')
@@ -61,14 +57,33 @@ section_label.place(x=100, y=145)
 semester_entry.place(x=250, y=100)
 section_entry.place(x=250, y=150)
 
+sec=0
 gpa=[]
+cgpa = []
 roll=[]
 name=[]
+pd=[]
+eem=[]
+dbms=[]
+se=[]
+daa=[]
+flat=[]
+dbms_lab=[]
+uml_lab=[]
 
+pd_fail=0
+eem_fail=0
+dbms_fail=0
+se_fail=0
+daa_fail=0
+flat_fail=0
+dbms_lab_fail=0
+uml_lab_fail=0
 
 def search():
     maxStudents = 0
     passPercent = 0
+    avgSGPA = 0
     avgCGPA = 0
     #Checking for empty fields
     if semester_entry.get() == '':
@@ -91,28 +106,13 @@ def search():
         tkinter.messagebox.showinfo('Error',"Section value cannot be greater than 19")
         section_entry.delete(0,'end')
         return
-
+    global sec
     sec = section_ip1
     sem = semester_ip1
 
     tkinter.messagebox.showinfo('NOTE','Running Chrome to get Results.Click --OK-- to proceed.')
     tkinter.messagebox.showinfo('NOTE','Please wait while we fetch data and record it. \nIMP: Donot close CHROME during operation.')
     # Initializing webdriver with its origin
-    if 'selenium' in get_installed_distributions():
-        pass
-    else:
-        live_status.set('Installing selenium')
-        window.update()
-        time.sleep(5)
-        os.system('pip install selenium')
-    if 'requests' in get_installed_distributions():
-        pass
-    else:
-        live_status.set('Installing requests')
-        window.update()
-        time.sleep(5)
-        os.system('pip install requests')
-    
     path = 'chromedriver.exe'
     if os.path.exists(path):
         pass
@@ -141,8 +141,9 @@ def search():
             browser.close()
             passPercent /=maxStudents
             passPercent *=100
+            avgSGPA /= maxStudents
             avgCGPA /= maxStudents
-            tkinter.messagebox.showinfo('Results','Students strength : '+str(maxStudents)+'\nPassPercentage: '+str(round(passPercent,2))+'\nAverege CGPA: '+str(round(avgCGPA,2)))
+            tkinter.messagebox.showinfo('Results','Students strength : '+str(maxStudents)+'\nPassPercentage: '+str(round(passPercent,2))+'\nAverege SGPA: '+str(round(avgSGPA,2))+'\nAverage CGPA: '+str(round(avgCGPA,2)))
             tkinter.messagebox.showinfo('NOTE',"The program has completed it's task, You can please press quit button to close the session")
             break
         semester = Select(browser.find_element_by_id("cbosem"))
@@ -172,35 +173,95 @@ def search():
             print("Failed")
 
             # automates and accepts alert by clicking "OK" button
-            gpa.append("NO_GPA_Recorded")
+            gpa.append("NO_SGPA_Recorded")
+            cgpa.append("NO_CGPA_Recorded")
             roll.append("Page_Error")
             name.append("Page_Error")
+            pd.append("Page_Error")
+            eem.append("Page_Error")
+            dbms.append("Page_Error")
+            se.append("Page_Error")
+            daa.append("Page_Error")
+            flat.append("Page_Error")
+            dbms_lab.append("Page_Error")
+            uml_lab.append("Page_Error")
+
             continue  # continues to next roll no.
         except TimeoutException:
             print("Successful!")
         Name = browser.find_element_by_id("lblname")
-        marks = browser.find_element_by_id("lblgpa")
+        pd1=browser.find_element_by_xpath("//*[@id='GridView1']/tbody/tr[2]/td[4]")
+        eem1=browser.find_element_by_xpath("//*[@id='GridView1']/tbody/tr[3]/td[4]")
+        dbms1=browser.find_element_by_xpath("//*[@id='GridView1']/tbody/tr[4]/td[4]")
+        se1=browser.find_element_by_xpath("//*[@id='GridView1']/tbody/tr[5]/td[4]")
+        daa1=browser.find_element_by_xpath("//*[@id='GridView1']/tbody/tr[6]/td[4]")
+        flat1=browser.find_element_by_xpath("//*[@id='GridView1']/tbody/tr[7]/td[4]")
+        dbms_lab1=browser.find_element_by_xpath("//*[@id='GridView1']/tbody/tr[8]/td[4]")
+        uml_lab1=browser.find_element_by_xpath("//*[@id='GridView1']/tbody/tr[9]/td[4]")
+        marks=browser.find_element_by_id("lblgpa")
+        marks1=browser.find_element_by_id("lblcgpa")
         regNo = browser.find_element_by_id("lblregdno")
+        #TESTING FAILS AND SUMMING
+        global pd_fail,eem_fail,dbms_fail,flat_fail,se_fail,daa_fail,dbms_lab_fail,uml_lab_fail
+        if pd1.text=='F':
+            pd_fail=pd_fail+1
+
+        if eem1.text=='F':
+            eem_fail=eem_fail+1
+
+        if dbms1.text=='F':
+            dbms_fail=dbms_fail+1
+
+        if se1.text=='F':
+            se_fail=se_fail+1
+
+        if daa1.text=='F':
+            daa_fail=daa_fail+1
+
+        if flat1.text=='F':
+            flat_fail=flat_fail+1
+
+        if dbms_lab1.text=='F':
+            dbms_lab_fail=dbms_lab_fail+1
+        if uml_lab1.text=='F':
+            uml_lab_fail=uml_lab_fail+1
+
+        print(pd_fail,eem_fail,dbms_fail,flat_fail,se_fail,daa_fail,dbms_lab_fail,uml_lab_fail)
         # takes text of the HTML element and assigns it to variable
-        cgpa = marks.text
-        gpa.append(cgpa)
+        pd.append(pd1.text)
+        eem.append(eem1.text)
+        dbms.append(dbms1.text)
+        se.append(se1.text)
+        daa.append(daa1.text)
+        flat.append(flat1.text)
+        dbms_lab.append(dbms_lab1.text)
+        uml_lab.append(uml_lab1.text)
+        sgpa=marks.text
+        gpa.append(sgpa)
+        t_cgpa = marks1.text
+        cgpa.append(t_cgpa)
         roll.append(regNo.text)
         name.append(Name.text)
+        
+
         back_elem = browser.find_element_by_id('Button1')
         back_elem.click()
         live_status.set('Extracted Roll no '+str(i))
         maxStudents = i
-        if  float(cgpa)> 0:
+        if  float(sgpa)> 0:
             passPercent +=1
-        avgCGPA += float(cgpa)
-        # creates data_all.csv and makes it read and write enable
+        avgSGPA += float(sgpa)
+        avgCGPA += float(t_cgpa)
+
+        # creates data_sec.csv and makes it read and write enable
         with open("data_"+str(sec)+".csv", "w+") as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(["Name", "Roll No", "Marks"])
+            writer.writerow(["Name", "Roll No", "PD", "EEM","DBMS","SE","DAA","FLAT","DBMS LAB","UML LAB","SGPA","CGPA"])
             for i in range(len(gpa)):
                 # Writing fetched data into csv file
-                writer.writerow([name[i], str(roll[i]), str(gpa[i])])
-
+                writer.writerow([name[i], str(roll[i]),str(pd[i]),str(eem[i]),str(dbms[i]),str(se[i]),str(daa[i]),str(flat[i]),str(dbms_lab[i]),str(uml_lab[i]),str(gpa[i]),str(cgpa[i])])
+            writer.writerow(["PD FAIL", "EEM FAIL","DBMS FAIL","SE FAIL","DAA FAIL","FLAT FAIL","DBMS LAB FAIL","UML LAB FAIL"])
+            writer.writerow([pd_fail,eem_fail,dbms_fail,se_fail,daa_fail,flat_fail,dbms_lab_fail,uml_lab_fail])
 
 #Submit_Button
 submit_button = Button(frame2, text="Submit", fg="green", bg="white", command=search,height=1,width=8,font="consolas 15 bold")
