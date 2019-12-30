363e096b8fbc02a81bedecea7ad0bedd48233818
KedarisettiSreeVamsi <34485436+KedarisettiSreeVamsi@users.noreply.github.com>
2019-12-30 16:19:09 +0530

    Update and rename init.py to setup.py

--- init.py
+++
@@ -1,6 +0,0 @@
-from pip._internal.utils.misc import get_installed_distributions
-import os
-if 'selenium' in get_installed_distributions():
-    os.system('pip install selenium')
-if 'requests' in get_installed_distributions():
-    os.system('pip install requests')
